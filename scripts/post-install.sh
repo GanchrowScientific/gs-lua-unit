1a542dbb8b8821c792db7be14268fa09f983dbba
Andy Ganchrow <andy.addington@ganchrow.com>
2017-11-24 16:17:57 -0800

    Add scripts to run luacheck on files
    Change-Id: I8e0b56c27f0900af0aed01cea9b4b067196ab5ff
    Reviewed-on: http://gerrit.ganchrow.com:8080/5203
    Reviewed-by: Andy Addington <andy.addington@ganchrow.com>
    Tested-by: Andy Addington <andy.addington@ganchrow.com>

--- scripts/post-install.sh
+++ scripts/post-install.sh
@@ -1,39 +1,12 @@
 #!/bin/bash
 
 PROJECT_PATH="$(dirname $0)/../"
-FILES=$(/bin/bash -c "find {src,test,integration-test} -follow -name *.ts")
-echo $FILES
 
-cd $PROJECT_PATH
-mkdir -p target/configs
-
-echo Installing in `pwd`
-
-echo Running tsc...
-node_modules/.bin/tsc
-COMPILATION=$?
-
-echo Running tslint..
-node_modules/.bin/tslint $FILES
-TS_LINTING=$?
+. ./scripts/lua-setup
 
 echo Running luacheck
 luacheck `find lua* -name *.lua`
 LUA_LINTING=$?
 
 
-echo Copying compiled source to helpers folder
-mkdir helpers
-cp -r target/dist/src/* ./helpers
-cp -r configs target/dist
-
-echo Fixing the source maps
-SOURCE_MAPS=0
-for f in $(find ./helpers/ -type f -name '*.js.map');
-do
-  echo "Fix: $f";
-  sed -i -e s_\\.\\./\\.\\./__ $f
-  (( SOURCE_MAPS += $? ))
-done
-
-exit $(( $COMPILATION + $TS_LINTING + $LUA_LINTING ))
+exit $(( $LUA_LINTING ))
